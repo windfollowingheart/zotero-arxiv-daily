e9fb41d16b37c5bbd63e205b78d1b8d315013421
windfollowingheart <132692774+windfollowingheart@users.noreply.github.com>
2025-12-19 15:02:55 +0800

    Comment out Arxiv paper retrieval and processing
    Comment out the code for retrieving and processing Arxiv papers.

--- main.py
+++ main.py
@@ -176,25 +176,26 @@ if __name__ == '__main__':
         corpus = filter_corpus(corpus, args.zotero_ignore)
         logger.info(f"Remaining {len(corpus)} papers after filtering.")
     logger.info("Retrieving Arxiv papers...")
-    papers = get_arxiv_paper(args.arxiv_query, args.debug)
-    if len(papers) == 0:
-        logger.info("No new papers found. Yesterday maybe a holiday and no one submit their work :). If this is not the case, please check the ARXIV_QUERY.")
-        if not args.send_empty:
-          exit(0)
-    else:
-        logger.info("Reranking papers...")
-        papers = rerank_paper(papers, corpus)
-        if args.max_paper_num != -1:
-            papers = papers[:args.max_paper_num]
-        if args.use_llm_api:
-            logger.info("Using OpenAI API as global LLM.")
-            set_global_llm(api_key=args.openai_api_key, base_url=args.openai_api_base, model=args.model_name, lang=args.language)
-        else:
-            logger.info("Using Local LLM as global LLM.")
-            set_global_llm(lang=args.language)
+    # papers = get_arxiv_paper(args.arxiv_query, args.debug)
+    # if len(papers) == 0:
+    #     logger.info("No new papers found. Yesterday maybe a holiday and no one submit their work :). If this is not the case, please check the ARXIV_QUERY.")
+    #     if not args.send_empty:
+    #       exit(0)
+    # else:
+    #     logger.info("Reranking papers...")
+    #     papers = rerank_paper(papers, corpus)
+    #     if args.max_paper_num != -1:
+    #         papers = papers[:args.max_paper_num]
+    #     if args.use_llm_api:
+    #         logger.info("Using OpenAI API as global LLM.")
+    #         set_global_llm(api_key=args.openai_api_key, base_url=args.openai_api_base, model=args.model_name, lang=args.language)
+    #     else:
+    #         logger.info("Using Local LLM as global LLM.")
+    #         set_global_llm(lang=args.language)
 
     html = render_email(papers)
     logger.info("Sending email...")
+    print(args.sender, args.receiver, args.sender_password, args.smtp_server, args.smtp_port, html)
     send_email(args.sender, args.receiver, args.sender_password, args.smtp_server, args.smtp_port, html)
     logger.success("Email sent successfully! If you don't receive the email, please check the configuration and the junk box.")
 
